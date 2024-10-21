5638a56b47f290a614006b80ba0f55316b782ec4
SayedFayab <sayedfayab@gmail.com>
2024-10-21 18:25:30 -0400

    project structure is added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Soruce_code_analysis',
+    version= '0.0.1',
+    author= 'Sayed Fayaz',
+    author_email= 'fayab.fayaz@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
